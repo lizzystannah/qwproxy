e477c79c304e88df09f83094545966f29021adc4
ellys <elizangelomanuel@gmail.com>
2026-05-14 23:00:59 +0100

    Update test_api.js to use native fetch

--- test_api.js
+++ test_api.js
@@ -1,7 +1,5 @@
-import fetch from 'node-fetch';
-
 const API_URL = 'https://quantterm-qwen.ikicmr.easypanel.host/v1/chat/completions';
-const API_KEY = 'SUA_CHAVE_AQUI'; // Coloque a chave que você definiu no Easypanel
+const API_KEY = 'SUA_CHAVE_AQUI'; // Lembre-se de trocar pela sua chave!
 
 async function test() {
   console.log('Enviando requisição para a API...');
